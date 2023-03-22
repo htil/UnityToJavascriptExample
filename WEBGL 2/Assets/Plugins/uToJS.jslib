f9881e9c6e2a423c1df2b9ce8e5d09279bacf64c
tkadams1 <46609177+tkadams1@users.noreply.github.com>
2023-03-22 15:21:15 -0500

    Fixed issue with string being sent to index.html by converting to bytecode in unity

--- WEBGL 2/Assets/Plugins/uToJS.jslib
+++ WEBGL 2/Assets/Plugins/uToJS.jslib
@@ -1,13 +1,14 @@
 mergeInto(LibraryManager.library, {
-
   HelloString: function (str) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-    var myDiv = document.getElementById("UnityToJavascript");
-    myDiv.innerHTML = UTF8ToString(str);
+        console.log("readyState: " + xhttp.readyState + " status: " + xhttp.status);
+      if (xhttp.readyState === 4) {
+        var myDiv = document.getElementById("UnityToJavascript");
+        myDiv.innerHTML = UTF8ToString(str);
+      }
     };
     xhttp.open("GET", "../../Build/index.html", true);
-    xhttp.send();
+    xhttp.send(str); // Pass the str parameter as part of the request
   },
-
 });
--- WEBGL 2/Assets/uToJS.cs
+++ WEBGL 2/Assets/uToJS.cs
@@ -1,27 +1,15 @@
+using System.Text;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
 public class uToJS : MonoBehaviour {
 
-    //[DllImport("__Internal")]
-    //private static extern void Hello();
-
     [DllImport("__Internal")]
-    private static extern void HelloString(string str);
-
-    //[DllImport("__Internal")]
-    //private static extern void PrintFloatArray(float[] array, int size);
-
-    //[DllImport("__Internal")]
-   // private static extern int AddNumbers(int x, int y);
-
-    //[DllImport("__Internal")]
-    //private static extern string StringReturnValueFunction();
-
-    //[DllImport("__Internal")]
-    //private static extern void BindWebGLTexture(int texture);
+    private static extern void HelloString(byte[] str);
 
     public void HelloWorldCall() {
-        HelloString("Hello World!");
+        string message = "Hello World!";
+        byte[] utf8Bytes = Encoding.UTF8.GetBytes(message);
+        HelloString(utf8Bytes);
     }
 }
